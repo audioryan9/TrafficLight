9586b79806f2be28f8da7d34ee25787899f609be
audioryan9 <ryandsgn@outlook.com>
2019-10-10 15:11:16 +0000

    first push

---
+++ src/js/component/TrafficLight.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+
+<>
+<div>
+    <div id="mainLight"  class="mainLite">aaaa
+        <span id="greenOne" class= "green1"> grn </span>
+        <span id="yellowOne" class= "yellow1"> yell </span>
+        <span id="redOne" class= "red1"> red </span>
+    </div>
+                <div id="walkLight"  class="walkLite">wlklite
+                    <span id="walk" class= "walk"> wlk </span>
+                    <span id="doNotWalk" class= "doNotWalk"> don't </span>
+                </div>
+
+                        <div id="configBox"  class="configBoxx">enable or disbale
+                            <span id="walk" class= "walk"> wlk </span>
+                            <span id="doNotWalk" class= "doNotWalk"> don't </span>
+                        </div>
+
+</div>
+</>
+
+function TrafficLight(){
+const [mainLight , setMainLight] = useState ([]);
+
+    return(
+        <div className= "Lite"/>
+
+
+
+
+ )};
+
+export default TrafficLight;
