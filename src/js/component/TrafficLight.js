1a0bafff712c0332572e9fecfef08ff7c2e53d2d
Trenton Knight <tknightj1995@yahoo.com>
2019-10-10 15:57:03 +0000

    one

--- src/js/component/TrafficLight.js
+++ src/js/component/TrafficLight.js
@@ -1,35 +1,38 @@
 import React from "react";
 
+function TrafficLight() {
+	//const [mainLight, setMainLight] = useState([]);
 
-<>
-<div>
-    <div id="mainLight"  class="mainLite">aaaa
-        <span id="greenOne" class= "green1"> grn </span>
-        <span id="yellowOne" class= "yellow1"> yell </span>
-        <span id="redOne" class= "red1"> red </span>
-    </div>
-                <div id="walkLight"  class="walkLite">wlklite
-                    <span id="walk" class= "walk"> wlk </span>
-                    <span id="doNotWalk" class= "doNotWalk"> don't </span>
-                </div>
+	return (
+		<div>
+			<div id="mainLight" className="mainLite">
+				aaaa
+				<div id="green" className="green1">
+					{" "}
+					grn{" "}
+				</div>
+				<div id="yellow" className="yellow1">
+					{" "}
+					yell{" "}
+				</div>
+				<div id="red" className="red1">
+					{" "}
+					red{" "}
+				</div>
+			</div>
 
-                        <div id="configBox"  class="configBoxx">enable or disbale
-                            <span id="walk" class= "walk"> wlk </span>
-                            <span id="doNotWalk" class= "doNotWalk"> don't </span>
-                        </div>
+			<div id="walkLight" className="walkLite">
+				<span id="walk" className="walk" />
+				<span id="doNotWalk" className="doNotWalk" />
+			</div>
 
-</div>
-</>
-
-function TrafficLight(){
-const [mainLight , setMainLight] = useState ([]);
-
-    return(
-        <div className= "Lite"/>
-
-
-
-
- )};
+			<div id="configBox" className="configBoxx">
+				enable or disbale
+				<span id="walk" className="walk" />
+				<span id="doNotWalk" className="doNotWalk" />
+			</div>
+		</div>
+	);
+}
 
 export default TrafficLight;
